f79b16b8ae8f3a276b13cce1a9d20723b8e78dfe
CFSpearing <135135070+CFSpearing@users.noreply.github.com>
2023-07-17 13:19:50 -0400

    uh the thing
    A

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,43 @@
 <script>
-	import { Canvas, InteractiveObject, OrbitControls, T } from '@threlte/core'
+	//import { Canvas, InteractiveObject, OrbitControls, T } from '@threlte/core'
 	import { spring } from 'svelte/motion'
 	import { degToRad } from 'three/src/math/MathUtils.js'
   import Header from './Header.svelte'
   import Chat from './Chat.svelte'
+  import {T, Canvas, PerspectiveCamera, AmbientLight, OrbitControls, InteractiveObject} from '@threlte/core';
+  import Hexglobe from "./lib/Hexglobe.svelte";
 
 
+
+  let hexData;
+  
+  fetch('./countries.geo.json')
+    .then((response) => response.json())
+    .then((data) => {
+      hexData = data.features;
+    })
+    .catch((error) => {
+      console.log(error);
+      return [];
+    });
+    
 	const scale = spring(1)
 </script>
 
-<div>
+<div class="split left">
+	<div class="centered">
   <Header/>
   <Chat/>
   </div>
+  </div>
 
-<div>
+<div class="split right">
 	<Canvas>
-		<T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
-			<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={false} target={{ y: 0.5 }} />
-		</T.PerspectiveCamera>
-
-		<T.DirectionalLight castShadow position={[3, 10, 10]} />
-		<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
-		<T.AmbientLight intensity={0.2} />
-
-		<!-- Cube -->
-		<T.Group scale={$scale}>
+		<PerspectiveCamera fov={90} position={{ x: 0, y:0, z: -12 }}>
+			<OrbitControls  autoRotate autoRotateSpeed={0.8} />
+		  </PerspectiveCamera>
+		  <AmbientLight intensity={0.9}  />
+		  <T.Group>
 			<T.Mesh position.y={0.5} castShadow let:ref>
 				<!-- Add interaction -->
 				<InteractiveObject
@@ -34,22 +46,46 @@
 					on:pointerenter={() => ($scale = 2)}
 					on:pointerleave={() => ($scale = 1)}
 				/>
-
-				<T.BoxGeometry />
-				<T.MeshStandardMaterial color="#333333" />
+				<T.SphereGeometry />
+				<T.MeshStandardMaterial color="white" />
 			</T.Mesh>
-		</T.Group>
-
-		<!-- Floor -->
-		<T.Mesh receiveShadow rotation.x={degToRad(-90)}>
-			<T.CircleGeometry args={[3, 72]} />
-			<T.MeshStandardMaterial color="white" />
-		</T.Mesh>
+			</T.Group>
+		  {#if hexData}
+			<Hexglobe hexdata={hexData}/>
+		  {/if}
 	</Canvas>
 </div>
-<style>
-	div {
-		height: 100%;
-		width: 100%;
-	}
+<style>    
+ /* Split the screen in half */
+ .split {
+  height: 100%;
+  width: 70%;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+/* Control the left side */
+.left {
+  left: 0;
+  /* background-color: #111; */
+}
+
+/* Control the right side */
+.right {
+  right: 0;
+  /* background-color: red; */
+}
+
+/* If you want the content centered horizontally and vertically */
+.centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
 </style>
---
+++ src/lib/data.js
@@ -0,0 +1,4 @@
+
+import { Material, SphereGeometry } from "three";
+import { writable } from "svelte/store";
+export const displayData = writable(null);
